d0dcd4039ec3da20d154ab9e3751f54d2df22833
AlFahadMunna <afmunna.afm.afm@gmail.com>
2025-11-20 10:50:26 -0600

    session routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,8 @@ import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { inngest, functions } from "./lib/inngest.js";
-import { protectRoute } from "./middleware/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
@@ -19,13 +20,11 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+app.use("/api/sessions", sessionRoutes);
 
 app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "success from api" });
-});
-
-app.get("/video-call", protectRoute, (req, res) => {
-  res.status(200).json({ msg: "this is a protected route" });
+  res.status(200).json({ message: "success from api" });
 });
 
 // make our app ready for deployment
